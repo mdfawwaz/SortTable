44b0682eea380fe763837572df4b6da24e2dc7c9
mdfawwaz <89119549+mdfawwaz@users.noreply.github.com>
2023-09-23 20:28:01 +0530

    Add files via upload

---
+++ New folder/hide.js
@@ -0,0 +1,52 @@
+function hideIntro() {
+    var x = document.getElementById("intro");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hideEdu() {
+    var x = document.getElementById("Edu");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function hideCerts() {
+    var x = document.getElementById("cert");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function hideSkill() {
+    var x = document.getElementById("skill");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function hideSkills() {
+    var x = document.getElementById("skills");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } function hideAch() {
+    var x = document.getElementById("ach");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  
+  
